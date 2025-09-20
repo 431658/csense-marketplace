9d817e5cd2f8dd25cbc01f7d4c58d2ef14d7baa6
不想上学 <bxsx0@outlook.com>
2025-09-20 16:55:20 +0800

    上传cjbancs.js
    破解chen-jin的反CSense扩展
    https://assets.ccw.site/extension/cjbancs

---
+++ cjbancs.js
@@ -0,0 +1,25 @@
+// 留给chen-jin的话：哈哈我破解了你的反CSense扩展😂
+Scratch.extensions.register({ getInfo() { return { id: "ceb3" } } });
+Scratch.extensions.register({
+    getInfo() {
+        return {
+            id: "cjbancs",
+            blocks: [
+                {
+                    opcode: "aiosvohvsabhv",
+                    blockType: Scratch.BlockType.BOOLEAN,
+                },
+                {
+                    opcode: "asdibousdsfad",
+                    blockType: Scratch.BlockType.BOOLEAN,
+                },
+            ],
+        };
+    },
+    aiosvohvsabhv() {
+        return true;
+    },
+    asdibousdsfad() {
+        return false;
+    },
+});
